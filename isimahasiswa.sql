ccbb670f4707eb574e173fdf7ed077e30867443f
MOHAMMDANHAMDAN <mohhamdan738@gmail.com>
2019-02-23 21:10:57 -0800

    Create isimahasiswa.sql

---
+++ isimahasiswa.sql
@@ -0,0 +1,7 @@
+Insert into mahasiswa(NIM, Nama, AlamatJalan, Kota, KodePos, noHP, JenisKelamin, TanggalLahir, KodeDosen)
+	values('11223344', 'Ari Santoso', 'jl. mawar', 'Bekasi', '501070', '0897433426', 'Laki-Laki', '1998-10-12', '48767'),
+	('11223345', 'Ario Talib', 'jl. melati', 'Cikarang', '501071', '0897433427', 'Laki-Laki', '1999-11-16', '48768'),
+	('11223346', 'Dina Marlina', 'jl. puspa', 'Karawang', '501072', '0897433428', 'Perempuan', '1997-12-01', '48769'),
+	('11223347', 'Lisa Ayu', 'jl. daun', 'Bekasi', '501073', '0897433429', 'Perempuan', '1996-01-02', '48770'),
+	('11223348', 'Tiara Wahidah', 'jl. kancil', 'Bekasi', '501074', '0897433430', 'Perempuan', '1980-02-05', '48771'),
+	('11223349', 'Anton Sinaga', 'jl. beruang', 'Cikarang', '501074', '0897433431', 'Laki-Laki', '1988-03-10', '48772'); 
